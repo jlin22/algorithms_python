da90e5b21f66e33e5a80b68ff14442f22f574b5d
John Lin <jlin1674@gmail.com>
2018-10-27 23:34:54 -0400

    finished BubbleSort

--- BubbleSort.py
+++ BubbleSort.py
@@ -1,6 +1,3 @@
-def BubbleSort(array):
-    """Uses bubble sort to return the sorted array"""
-
 def sorted(array):
     """Returns a boolean of whether the array is sorted or not"""
     for i in range(len(array) - 1):
@@ -8,8 +5,26 @@ def sorted(array):
             return False
     return True
 
+def swap(array, p, q):
+    temp = array[p]
+    array[p] = array[q]
+    array[q] = temp
+
+def BubbleSort(array):
+    """Uses bubble sort to return the sorted array"""
+    for i in reversed(range(len(array))):
+        #the array from i + 2 to the end is already sorted
+        for j in range(i):
+            if array[j] > array[j + 1]:
+               swap(array, j, j + 1)
+        #array[i + 1] (the end) is in the correct spot, along with i + 2 to the end
+    assert(sorted(array))
+
 unsorted = [4, 16, 124125, 6, 45, 1, 9]
 sorted_array = [1, 5, 12, 21, 51, 65, 89, 120, 159, 189]
 print(sorted(unsorted))
 print(sorted(sorted_array))
+BubbleSort(unsorted)
+print(unsorted)
+
 
